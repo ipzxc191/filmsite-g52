1b134ffa49dc1f53376d09251b4b6d5263bceb6e
ipotnin <desertigorb@gmail.com>
2026-06-29 16:19:28 +0300

    feat: add views (film_list, film_detail, director_detail); add urls (films/, films/id, directors/id)

--- films/urls.py
+++ films/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('films/', views.film_list, name='film_list'),
+    path('films/<int:film_id>/', views.film_detail, name='film_detail'),
+    path('directors/<int:director_id>/', views.director_detail, name='director_detail'),
 ]
--- films/views.py
+++ films/views.py
@@ -1,3 +1,4 @@
+# films/views.py
 from django.http import HttpResponse
 
 
@@ -7,3 +8,14 @@ def index(request):
 
 def about(request):
     return HttpResponse('Сайт-каталог фильмов. Здесь собраны лучшие фильмы всех времён.')
+
+
+def film_list(request):
+    return HttpResponse('Список фильмов — скоро здесь будет каталог.')
+
+
+def film_detail(request, film_id):
+    return HttpResponse(f'Страница фильма с id={film_id}')
+
+def director_detail(request, director_id):
+    return HttpResponse(f'Страница режиссёра с id={director_id}')
