608b2e5e5a513ebc3aeab10fafdd784c954ce222
ipotnin <desertigorb@gmail.com>
2026-07-04 23:38:11 +0300

    feat: base template, templates films includes

--- films/urls.py
+++ films/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'films'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('films/', views.film_list, name='film_list'),
-    path('films/search/', views.search_film, name='search_film'),
     path('films/<int:film_id>/', views.film_detail, name='film_detail'),
     path('films/add/', views.add_film, name='add_film'),
+    path('films/search/', views.search_film, name='search_film'),
     path('directors/<int:director_id>/', views.director_detail, name='director_detail'),
 ]
