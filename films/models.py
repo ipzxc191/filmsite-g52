1f738de7a548a749568714c481540a98b4052798
ipotnin <desertigorb@gmail.com>
2026-07-08 16:10:40 +0300

    feat: add models Film, Genre

--- films/models.py
+++ films/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    year = models.PositiveIntegerField(verbose_name='Год выпуска')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        default=0.0,
+        verbose_name='Рейтинг'
+    )
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        ordering = ['-year']
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+    def __str__(self):
+        return f'{self.title} ({self.year})'
